6bdd551ce0ec52a82681e3a86b87fef70bb44d66
Atoyebea <144946811+Eatoyebi@users.noreply.github.com>
2024-04-01 01:58:59 -0400

    Part 3 of tutorial complete

--- game.js
+++ game.js
@@ -1,3 +1,12 @@
 window.onload = function() {
-    var game = new Phaser.Game();
+    var config = {
+        width: 256,
+        height: 272,
+        backgroundColor: 0x000000,
+        scene: [Scene1, Scene2],
+        pixelArt: true
+    }
+
+
+    var game = new Phaser.Game(config);
 }
