864b5184ed9fb0177afcbe20004501a105c15a19
Atoyebea <144946811+Eatoyebi@users.noreply.github.com>
2024-04-01 05:17:12 -0400

    part 9 complete

--- Scene2.js
+++ Scene2.js
@@ -6,9 +6,14 @@ class Scene2 extends Phaser.scene {
         this.background = this.add.tileSprite(0,0, config.width, config.height, "background)");
         this.background.setOrigin(0, 0);
 
-        this.ship1 = this.add.sprite(config.width / 2 - 50, config.height /2, "ship")
-        this.ship2 = this.add.sprite(config.width / 2, config.height /2, "ship2")
-        this.ship3 = this.add.sprite(config.width / 2 + 50, config.height /2, "ship3")
+        this.ship1 = this.add.sprite(config.width / 2 - 50, config.height /2, "ship");
+        this.ship2 = this.add.sprite(config.width / 2, config.height /2, "ship2");
+        this.ship3 = this.add.sprite(config.width / 2 + 50, config.height /2, "ship3");
+
+        this.enemies = this.physics.add.group();
+        this.enemies.add(this.ship1);
+        this.enemies.add(this.ship2);
+        this.enemies.add(this.ship3);
 
         this.ship1.play("ship1_anim");
         this.ship2.play("ship2_anim");
@@ -52,6 +57,29 @@ class Scene2 extends Phaser.scene {
         this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
         this.projectiles = this.add.group();
+
+        this.physics.add.collifer(this.projectiles, this.powerUps, function(projectile, powerUp){
+            projectile.destroy();
+        });
+        
+        this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, null, this);
+        this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, null, this);
+        this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, null, this);
+    }
+
+    pickPowerUp(player, powerUp) {
+        powerUp.disableBody(true, true);
+    }
+
+    hurtPlayer(player, enemy) {
+        this.resetShipPos(enemy);
+        player.x = config.width / 2 - 8;
+        player.y = config.height - 64;
+    }
+
+    hitEnemy(projectile, enemy) {
+        projectile.destroy();
+        this.resetShipPos(enemy);
     }
 
     update() {
