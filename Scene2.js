46a27a34d516ecbf06a4ed47a82ef91b261ed049
Atoyebea <144946811+Eatoyebi@users.noreply.github.com>
2024-04-01 05:55:03 -0400

    complete

--- Scene2.js
+++ Scene2.js
@@ -83,11 +83,21 @@ class Scene2 extends Phaser.scene {
 
     hurtPlayer(player, enemy) {
         this.resetShipPos(enemy);
-        player.x = config.width / 2 - 8;
-        player.y = config.height - 64;
+        if(this.player.alpha < 1){
+            return;
+        }
+        var explosion = new explosion(this, player.x, player.y);
+        player.disableBody(true, true);
+        this.time.addEvent({
+            delay: 1000,
+            callback: this.resetPlayer,
+            callbackScope: this,
+            loop: false
+        })
     }
 
     hitEnemy(projectile, enemy) {
+        var explosion = new explosion(this, enemy.x, enemy.y);
         projectile.destroy();
         this.resetShipPos(enemy);
         this.score += 15;
@@ -102,7 +112,9 @@ class Scene2 extends Phaser.scene {
         this.background.tilePositionY -= 0.5;
         this.movePlayerManager();
         if(Phaser.Input.Keyboard.JustDown(this.spacebar)) {
-            this.shootBeam();
+            if(this.player.active){
+                this.shootBeam();
+            }
         }
         for(var i =0; i<this.projectiles.getChildren().lemgth; i++){
             var beam= this.projectiles.getChildren()[i];
@@ -151,5 +163,23 @@ class Scene2 extends Phaser.scene {
             stringNumber = "0" + stringNumber;
         }
         return stringNumber;
+    }
+
+    resetPlayer() {
+        var x = config.width / 2 - 8;
+        var y = config.height + 64;
+        this.player.enableBody(true, x, y, true, true);
+        this.player.alpha = 0.5;
+        var tween = this.tweens.add ({
+            targets: this.player,
+            y: config.height - 64,
+            ease: 'Power1',
+            duration: 1500,
+            repeat: 0,
+            onComplete: function(){
+                this.player.alpha = 1;
+            },
+            callbackScope: this
+        });
     }
 }
