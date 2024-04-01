2740c36c1e8e8d42ad17602ed3f5e1c90c4ec657
Atoyebea <144946811+Eatoyebi@users.noreply.github.com>
2024-04-01 05:01:20 -0400

    real part 8 and code cleaning

--- Screen2.js
+++ Screen2.js
@@ -9,30 +9,6 @@ class Scene2 extends Phaser.scene {
         this.ship1 = this.add.sprite(config.width / 2 - 50, config.height /2, "ship")
         this.ship2 = this.add.sprite(config.width / 2, config.height /2, "ship2")
         this.ship3 = this.add.sprite(config.width / 2 + 50, config.height /2, "ship3")
-        this.player = this.physics.add.sprite(config.width / 2-8, config.height - 64, "player");
-        this.player.play("thrust");
-        this.cursorKeys = this.input.keyboard.createCursorKeys();
-        this.player.setCollideWorldBounds(true);
-        this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-        this.powerUps = this.physics.add.group();
-
-        var maxObjects = 4;
-        for(var i = 0; i<= maxObjects; i++){
-            var powerUp = this.physics.add.sprite(16, 16, "power-up");
-            this.powerUps.add(powerUp);
-            powerUp.setRandomPosition(0,0, game.config.width, game.config.height);
-
-            if (Math.random() > 0.5) {
-                powerUp.play("red");
-            } else {
-                powerUp.play("gray");
-            }
-            powerUp.setVelocity(100, 100);
-            powerUp.setCollideWorldBounds(true);
-            powerUp.setBounce(1);
-        }
-        
 
         this.ship1.play("ship1_anim");
         this.ship2.play("ship2_anim");
@@ -43,12 +19,58 @@ class Scene2 extends Phaser.scene {
         this.ship3.setInteractive();
 
         this.input.on('gameobjectdown', this.destroyShip, this);
-
         this.add.text(20, 20, "Playing game" , 
         { 
             font: "25px Arial", 
             fill: "yellow"
         });
+        this.physics.world.setBoundsCollision();
+
+        this.powerUps = this.physics.add.group();
+
+
+        for(var i = 0; i<= gameSettings.maxPowerups; i++){
+            var powerUp = this.physics.add.sprite(16, 16, "power-up");
+            this.powerUps.add(powerUp);
+            powerUp.setRandomPosition(0,0, game.config.width, game.config.height);
+
+            if (Math.random() > 0.5) {
+                powerUp.play("red");
+            } else {
+                powerUp.play("gray");
+            }
+            powerUp.setVelocity(gameSettings.powerUpVel, gameSettings.powerUpVel);
+            powerUp.setCollideWorldBounds(true);
+            powerUp.setBounce(1);
+        }
+
+        this.player = this.physics.add.sprite(config.width / 2-8, config.height - 64, "player");
+        this.player.play("thrust");
+        this.player.setCollideWorldBounds(true);
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
+
+        this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        this.projectiles = this.add.group();
+    }
+
+    update() {
+        this.moveShip(this.ship1, 1);
+        this.moveShip(this.ship2, 2);
+        this.moveShip(this.ship3, 3);
+        this.background.tilePositionY -= 0.5;
+        this.movePlayerManager();
+        if(Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+            this.shootBeam();
+        }
+        for(var i =0; i<this.projectiles.getChildren().lemgth; i++){
+            var beam= this.projectiles.getChildren()[i];
+            beam.update();
+        }
+    }
+
+    shootBeam(){
+        var beam = new beam(this);
     }
 
     moveShip(ship, speed) {
@@ -57,16 +79,7 @@ class Scene2 extends Phaser.scene {
             this.resetShipPos(ship);
         }
     }
-    update() {
-        this.moveShip(this.ship1, 1);
-        this.moveShip(this.ship2, 2);
-        this.moveShip(this.ship3, 3);
-        this.background.tilePositionY -= 0.5;
-        this.movePlayerManager();
-        if(Phaser.Input,Keyboard.JustDown(this.spacebar)) {
-            console.log("Fire!");
-        }
-    }
+
     movePlayerManager(){
         if(this.cursorKeys.left.isDown) {
             this.player.setVelocityX(-gameSettings.playerSpeed);
--- game.js
+++ game.js
@@ -1,17 +1,20 @@
-window.onload = function() {
-    var config = {
-        width: 256,
-        height: 272,
-        backgroundColor: 0x000000,
-        scene: [Scene1, Scene2],
-        pixelArt: true,
-        physics: {
-            default: "arcade",
-            arcade: {
-                debug: false
-            }
+var gameSettings{
+    playerSpeed = 200, 
+    maxPowerUps = 2,
+    powerUpVels = 50,
+}
+var config = {
+    width: 256,
+    height: 272,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2],
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
         }
     }
+ }
 
-    var game = new Phaser.Game(config);
-}
+var game = new Phaser.Game(config);
